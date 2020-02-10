d607b843624c9f683fe2587fae5152faba87d6a3
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-02-10 15:24:01 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//LESSON 1 INTRO JS типи даних
+
 //string : '' "" ``
 //numder : 2, 12.2
 //boolean: true false
@@ -94,7 +96,7 @@
 
 
 
-//Lesson 2........................................................................
+//LESSON 2 JS INTRO........................................................................
 
 // JS Оператори 
 // +, -, /, *, % остача від ділення
@@ -208,29 +210,137 @@
 // alert(correct);
 
 
-let admin = 'admin';
-let user = 'user';
-let pro = 'pro';
+// let admin = 'admin';
+// let user = 'user';
+// let pro = 'pro';
 
 
-let passAdmin = 1111;
-let passUser = 2222;
-let passPro = 3333;
+// let passAdmin = 1111;
+// let passUser = 2222;
+// let passPro = 3333;
 
 
 
-let name = prompt('Write your status');
-let password = prompt('Write your password');
+// let name = prompt('Write your status');
+// let password = prompt('Write your password');
 
-if (name == admin && password == passAdmin) {
-    document.write(`<div style="width: 300px; height: 300px; background-color: red;
-    margin: 100px auto"> Welcome ${admin}</div>`)
-} else if ( name == user && password == passUser) {
-    document.write(`<div style="width: 300px; height: 300px; background-color: blue;
-    margin: 100px auto"> Welcome ${user}</div>`)
-}  else if ( name == pro && password == passPro) {
-    document.write(`<div style="width: 300px; height: 300px; background-color: green;
-    margin: 100px auto"> Welcome ${pro}</div>`)
-} else {
-    alert('Wrong pass or name')
-}
+// if (name == admin && password == passAdmin) {
+//     document.write(`<div style="width: 300px; height: 300px; background-color: red;
+//     margin: 100px auto"> Welcome ${admin}</div>`)
+// } else if ( name == user && password == passUser) {
+//     document.write(`<div style="width: 300px; height: 300px; background-color: blue;
+//     margin: 100px auto"> Welcome ${user}</div>`)
+// }  else if ( name == pro && password == passPro) {
+//     document.write(`<div style="width: 300px; height: 300px; background-color: green;
+//     margin: 100px auto"> Welcome ${pro}</div>`)
+// } else {
+//     alert('Wrong pass or name')
+// }
+
+
+
+//LESSON 3 JS INTRO цикли
+
+//for, while, dо while;
+
+// for(початок циклу; умова; крок){тіло циклу} 
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// let n = +prompt('Start');
+
+// for ( let i = n; i <= 100; i++) {
+//     console.log(i)
+// }
+
+
+// let start = +prompt('Start');
+// let end = +prompt('End');
+
+// for (let i = start; i < end; i++) {
+//     console.log(i);
+// }
+
+
+// for (let i = 4; i <= 400; i++) {
+//     console.log(i)
+// }
+
+
+// for (let i = 4; i <= 1000; i) {
+//     console.log(i)
+// }
+
+
+// for ( let i = 1000; i <= 2000; i++) {
+//     console.log('&#' + i)
+//     document.write(`&#${i}`)
+// }
+
+//виведення числа в + - степінь
+// let number = +prompt('Введіть число');
+// let pow = +prompt('Введіть степінь');
+// let  result = 1;
+// for (let i = 1; i <= pow; i++) {
+//     result =result*number;
+// }
+
+
+// if (pow > 0) {
+//     for (let i = 1; i <= pow; i++) {
+//     result =result*number;
+//     }
+// }else if(pow < 0) {
+//     pow = - pow;
+//     for (let i = 1; i <= pow; i++) {
+//     result = result*number;
+//     }
+//     result = 1 / result;
+// }
+
+// console.log(result);
+
+
+
+//додавання числа амого на себе
+// let from = +prompt('from');
+// let to = +prompt('to');
+// let result = 0;
+//через умови
+// let change;
+// if(from > to) {
+//     change = from;
+//     from = to;
+//     to = change;
+// }
+// for(let i = from; i <= to; i++) {
+//     result += i;
+// }
+// alert(result);
+//через два цикли
+// for(let i = from; i <= to; i++) {
+//     result = result + i;
+// }
+// for (let i = to; i <= from; i++) {
+//     result = result + i;
+// }
+
+//область видимості 
+// for (let i = 1; i <= 10; i++) {
+//     document.write(`<h2>Outside Loop ${i}</h2>`)
+//     for( let j = 1; j <= 3; j++) {
+//         document.write(`<p>Inside Loop ${j}</p>`)
+//     }
+// }
+
+
+// if(true) {
+//     let a = 10;
+//     console.log(a);// тут консолька виведе 
+// }
+// console.log(a) а тут не виведе, а поза межами
+
+
+
